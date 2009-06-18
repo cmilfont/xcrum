7c92aaa1213fbb14e478a43ec3a4080215f855bd
cmilfont <cmilfont@cmilfont.(none)>
2009-06-17 22:25:47 -0300

    teste

---
+++ db/development_structure.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL auto_increment,
+  `description` varchar(255) default NULL,
+  `project_id` int(11) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `login` varchar(40) default NULL,
+  `name` varchar(100) default '',
+  `email` varchar(100) default NULL,
+  `crypted_password` varchar(40) default NULL,
+  `salt` varchar(40) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  `remember_token` varchar(40) default NULL,
+  `remember_token_expires_at` datetime default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `index_users_on_login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO schema_migrations (version) VALUES ('20090513213438');
+
+INSERT INTO schema_migrations (version) VALUES ('20090603190217');
+
+INSERT INTO schema_migrations (version) VALUES ('20090610202931');
